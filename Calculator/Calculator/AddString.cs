d0dfd33b1ba9873679f76e75b1807315ef0d9c6e
WL12321 <81515546+WL12321@users.noreply.github.com>
2021-03-28 21:16:18 +0800

    Add files via upload

---
+++ Calculator/Calculator/AddString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class AddString:Calculator
+    {
+        string str1;
+
+        public string Str1
+        {
+            get { return str1; }
+            set { str1 = value; }
+        }
+        string str2;
+
+        public string Str2
+        {
+            get { return str2; }
+            set { str2 = value; }
+        }
+        public void addStr()
+        {
+            System.Text.StringBuilder st = new StringBuilder();
+            string str = str1 + str2;
+            Console.WriteLine("str1+str1:"+str);
+        }
+    }
+}
---
+++ Calculator/Calculator/Equal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Equal
+    {
+        public int Equals(int x, int y)
+        {
+            int flag;
+            if (x == y)
+            {
+                flag = 1;
+                Console.WriteLine(" Equal! ");
+            }
+            else
+            {
+                flag = 0;
+                Console.WriteLine(" Unequal! ");
+            }
+            return flag;
+        }
+    }
+}
